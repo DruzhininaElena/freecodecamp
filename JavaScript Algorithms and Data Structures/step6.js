ea9db4abfcf00fd7483abe9b4d924d282a2b5e1c
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-01 19:17:40 +0700

    step6

---
+++ JavaScript Algorithms and Data Structures/step6.js
@@ -0,0 +1,8 @@
+// Description:
+// After your console.log, assign the value "World" to your character variable.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = 'World';

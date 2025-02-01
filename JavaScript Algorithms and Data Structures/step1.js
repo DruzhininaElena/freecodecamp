08aabdc63896e66552b9bf1a47edabd20fa9dba9
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-01 18:40:50 +0700

    step1

---
+++ JavaScript Algorithms and Data Structures/step1.js
@@ -0,0 +1,14 @@
+// Description:
+// A pyramid generator is a program where you can set the type of character,
+//     the count for the pyramid, and the direction of the pyramid.
+//     The program will then generate a pyramid based on those inputs.
+
+// Solution:
+
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("     #     ");
+console.log("    ###    ");
+console.log("   #####   ");
+console.log("  #######  ");
+console.log(" ######### ");
+console.log("###########");

9b43d825d8b05179819315a0a1e5a794aee0823c
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-01 19:28:52 +0700

    step9-10

---
+++ JavaScript Algorithms and Data Structures/step10.js
@@ -0,0 +1,12 @@
+// Description:
+// Assign the string "Test" to your secondCharacter variable below your declaration.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+console.log(character);
+let secondCharacter;
+secondCharacter = 'Test';
+console.log(secondCharacter);

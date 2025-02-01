679e4096c7a4ea3330677ae2114b5dcba353ce6a
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-01 19:22:19 +0700

    step8

---
+++ JavaScript Algorithms and Data Structures/step8.js
@@ -0,0 +1,10 @@
+// Description:
+// Use camel case to declare a new secondCharacter variable.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+console.log(character);
+let secondCharacter;

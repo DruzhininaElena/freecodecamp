d0fb578d26692da23eeaaa31ebb362c4584a80db
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-02 20:07:22 +0700

    completed steps until 43

--- JavaScript Algorithms and Data Structures/building-a-pyramid-generator.js
+++ JavaScript Algorithms and Data Structures/building-a-pyramid-generator.js
@@ -3,9 +3,16 @@
 
 // Solution:
 
-let character = "Hello";
-console.log(character);
-character = "World";
-let secondCharacter;
-secondCharacter = "Test";
-console.log(secondCharacter);
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(i);
+}
+
+let result = ""
+
+for (const row of rows) {
+    result = result + row;
+}

f9c8dc5369b5ce53e33b874e2f2df319ccd074fa
Elena Druzhinina <elenashebrova@gmail.com>
2025-02-03 17:41:26 +0700

    completed steps until 62

--- JavaScript Algorithms and Data Structures/building-a-pyramid-generator.js
+++ JavaScript Algorithms and Data Structures/building-a-pyramid-generator.js
@@ -7,12 +7,21 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow() {
+    const test = "Testing";
+    return test;
+}
+const call = padRow();
+console.log(call);
+
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(i);
+    rows.push(character.repeat(i + 1))
 }
 
 let result = ""
 
 for (const row of rows) {
-    result = result + row;
+    result = result + row + "\n";
 }
+
+console.log(result);
